e69c42dc127876484cea89d1a15bf063b954c581
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2018-10-09 17:18:08 +0530

    HACKER RANK PROBLEM

---
+++ beautifulmoviesucsses.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p,num;
+int q,k;
+int i;
+int count=0;
+int rev=0;
+int rem;
+int a[1000];
+printf("enter the first elements");
+scanf("%d",&p);
+printf("enter the second elements");
+scanf("%d",&q);
+printf("enter the value of k");
+scanf("%d",&k);
+for(i=p;i<=q;i++)
+{
+    num=i;
+    rev=0;
+    while(num>0)
+    {
+        rem=num%10;
+        rev=rem+rev*10;
+        num=num/10;
+    }
+    num=i;
+    printf("%d\n",rev);
+    printf("%d\n",num);
+    if((rev-num)%k==0||rev-num==0)
+    {   printf("no is found\n");
+        count=count+1;
+    }
+}
+printf("%d\n",count);
+getch();
+}
+
