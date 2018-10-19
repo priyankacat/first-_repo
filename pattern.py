93736b8e313eb6b0684fb660672c5a651ea654b8
ayush2390 <43995654+ayush2390@users.noreply.github.com>
2018-10-20 00:23:49 +0530

    Update pattern.py
    i have made changes to make code user freindly

--- pattern.py
+++ pattern.py
@@ -1,4 +1,8 @@
-p=6
-for i in range(1,p):
-	print(i,end="*")
+p=int(input("enter the value of p"))
+q=p+10
+print(q)
+for i in range(1,q):
+	for j in range (1,p)
+		print(i,end="*")
+		print("#**##")
 
