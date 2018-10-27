3ab6c34bca32f2a7c888c03f2c5f25fec3ee672f
priyankacat <44523051+priyankacat@users.noreply.github.com>
2018-10-27 21:39:40 +0530

    Update sum.py
    i have made some specific changes

--- sum.py
+++ sum.py
@@ -1,9 +1,14 @@
 a=5
 b=10
-c=a+b
-d=c+b
-if(d>0):
+p=int(input("enter the number to be used")
+c=a+b+e
+print(c)
+d=c+b+e
+print(d)
+if(d>10):
   print("the complete answer")
+  print("no is greater than 10")
 else:
-  print("this is not the complete answer"
+  print("this is not the complete answer")
+  print("no is less than 10")
 print(c)
