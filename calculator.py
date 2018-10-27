5459bb820ab166e39941a8ec1e2dfa3b61b1d0f9
priyankacat <44523051+priyankacat@users.noreply.github.com>
2018-10-27 21:36:55 +0530

    Update calculator.py
    i have made changes to make graphic user interface better and code user freindly

--- calculator.py
+++ calculator.py
@@ -93,47 +93,47 @@ if __name__ == "__main__":
     # location inside the root window .
     # when user press the button, the command or
     # function affiliated to that button is executed .
-    button1 = Button(gui, text=' 1 ', fg='black', bg='red',
+    button1 = Button(gui, text=' 1 one', fg='black', bg='red',
                      command=lambda: press(1), height=1, width=7)
     button1.grid(row=2, column=0)
  
-    button2 = Button(gui, text=' 2 ', fg='black', bg='red',
+    button2 = Button(gui, text=' 2 two', fg='black', bg='red',
                      command=lambda: press(2), height=1, width=7)
     button2.grid(row=2, column=1)
  
-    button3 = Button(gui, text=' 3 ', fg='black', bg='red',
+    button3 = Button(gui, text=' 3 three', fg='black', bg='red',
                      command=lambda: press(3), height=1, width=7)
     button3.grid(row=2, column=2)
  
-    button4 = Button(gui, text=' 4 ', fg='black', bg='red',
+    button4 = Button(gui, text=' 4 four', fg='black', bg='red',
                      command=lambda: press(4), height=1, width=7)
     button4.grid(row=3, column=0)
  
-    button5 = Button(gui, text=' 5 ', fg='black', bg='red',
+    button5 = Button(gui, text=' 5 five', fg='black', bg='red',
                      command=lambda: press(5), height=1, width=7)
     button5.grid(row=3, column=1)
  
-    button6 = Button(gui, text=' 6 ', fg='black', bg='red',
+    button6 = Button(gui, text=' 6 six', fg='black', bg='red',
                      command=lambda: press(6), height=1, width=7)
     button6.grid(row=3, column=2)
  
-    button7 = Button(gui, text=' 7 ', fg='black', bg='red',
+    button7 = Button(gui, text=' 7 seven', fg='black', bg='red',
                      command=lambda: press(7), height=1, width=7)
     button7.grid(row=4, column=0)
  
-    button8 = Button(gui, text=' 8 ', fg='black', bg='red',
+    button8 = Button(gui, text=' 8 eight', fg='black', bg='red',
                      command=lambda: press(8), height=1, width=7)
     button8.grid(row=4, column=1)
  
-    button9 = Button(gui, text=' 9 ', fg='black', bg='red',
+    button9 = Button(gui, text=' 9 nine', fg='black', bg='red',
                      command=lambda: press(9), height=1, width=7)
     button9.grid(row=4, column=2)
  
-    button0 = Button(gui, text=' 0 ', fg='black', bg='red',
-                     command=lambda: press(0), height=1, width=7)
+    button0 = Button(gui, text=' 0 zero', fg='black', bg='red',
+                     command=lambda: press(0), height=2, width=8)
     button0.grid(row=5, column=0)
  
-    plus = Button(gui, text=' + ', fg='black', bg='red',
+    plus = Button(gui, text=' + plus ', fg='black', bg='red',
                   command=lambda: press("+"), height=1, width=7)
     plus.grid(row=2, column=3)
  
